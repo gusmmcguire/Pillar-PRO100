a539f309884a57282851d4b1d67d02b4aa9ad2ce
MergaTog <jlaing@student.neumont.edu>
2021-11-17 18:02:10 -0800

    Interactable is finalized

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    abstract public void Interact(GameObject InteractingObject);
+}
